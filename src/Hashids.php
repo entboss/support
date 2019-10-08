441c9d3604248883abde974b51bc13bf23a3ef8f
hexincorp <hexincorp@qq.com>
2019-10-08 11:25:33 +0800

    add Hashids

---
+++ src/Hashids.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Hashids
+ *
+ * @package    EntBoss
+ * @copyright  Copyright (c) 2019 EntBoss (http://www.entboss.com)
+ * @license    http://www.entboss.com/license
+ * @author     EntBoss Team
+ * @version    19.10.8
+ *
+ */
+
+namespace Eb\Support;
+
+class Hashids extends \Hashids\Hashids
+{
+
+}
